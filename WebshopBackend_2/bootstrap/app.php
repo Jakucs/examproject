09205cce1c7fabce4ac8d1b5a7f2cfac567005ec
Czuppon David <czuppondavid@yahoo.com>
2025-03-23 11:25:17 +0100

    noRatelimiter

--- WebshopBackend_2/bootstrap/app.php
+++ WebshopBackend_2/bootstrap/app.php
@@ -5,6 +5,12 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\RoleMiddleware;
 use App\Http\Middleware\EnsureProfileIsComplete;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\RateLimiter as FacadesRateLimiter;
+use Illuminate\Cache\RateLimiting\Limit; 
+use Illuminate\Cache\RateLimiter;   
+
 
 return Application::configure(basePath: dirname(__DIR__))
 
@@ -15,20 +21,25 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->api('throttle:api');
+        //$middleware->api('throttle:api');
         $middleware->api(\Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class);
     
          $middleware->alias([
 
         'ensure.profile.complete' => EnsureProfileIsComplete::class,
         'role' => RoleMiddleware::class,
-        
-
-            
+                   
         ]);
-    })
-    
 
+    })
+   /*Rate Limiter beállítása
+   ->withRateLimiter(function () {
+    FacadesRateLimiter::for('api', function (Request $request) {
+        // 60 kérés per perc
+        return Limit::perMinute(60);
+    });
+})  
+    */
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
