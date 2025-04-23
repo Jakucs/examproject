f17cf83ff70a5fa66f7d964bd0aa51cf1f028848
Jakucs Viktor <jakucs42@gmail.com>
2025-04-23 22:20:52 +0200

    final

--- frontend/src/app/shared/product-search.service.ts
+++ frontend/src/app/shared/product-search.service.ts
@@ -14,9 +14,9 @@ export class ProductSearchService {
   searchQuery$ = this.searchQuery.asObservable(); //A BehaviorSubject egyúttal Observer is! Nemcsak Observable! Így más komponensek nemtudják megváltoztatni (next()-el) csak feliratkozni rá!
 
 
-  //// Encapsulationt használunk. Ez egy OOP elv, amely arról szól, hogy egy objektum belső állapotát és működését elrejted a külvilág elől, és csak egy meghatározott interfészen keresztül teszed hozzáférhetővé.
+  //// Encapsulationt használunk. Ez egy OOP elv, amely arról szól, hogy egy objektum belső állapotát és működését elrejtjük a külvilág elől, és csak egy meghatározott interfészen keresztül tesszük hozzáférhetővé.
   updateSearchQuery(query: string){
     this.searchQuery.next(query); 
   }
-  //Az Angularban például ez történik, amikor egy komponenst vagy szolgáltatást úgy tervezel meg, hogy a belső állapotát (BehaviorSubject) csak egy függvény (updateSearchQuery()) módosíthatja.
+  //Az Angularban például ez történik, amikor egy komponenst vagy szolgáltatást úgy tervezünk meg, hogy a belső állapotát (BehaviorSubject) csak egy függvény (updateSearchQuery()) módosíthatja.
 }
